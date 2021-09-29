c7c691a4c750bbf81e1ad1dab3ac2f488be9520f
mmrmas <mmrmas@hotmail.com>
2021-09-29 22:21:41 +0800

    v0.12

--- loadModel.py
+++ loadModel.py
@@ -15,25 +15,23 @@ from torch.autograd import Variable
 
 file_types = [("JPEG (*.jpg)", "*.jpg"),
               ("All files (*.*)", "*.*")]
-#['rot', 'three_days', 'two_days'] from learner.dls.vocab
+labels     = ['rot', 'three_days', 'two_days'] #from learner.dls.vocab
 
 
 def main():
-
-    #learner = load_learner("/Users/samlinsen/Dropbox/biodiversity/training/BootcampAI/01/trainedBananaModel/bananaModel.pkl")
+    # the model was created by torch.save(learner.model,'drive/MyDrive/saved_model_bananas_torch/bananaModel2.pt') in the notebook
     model = torch.load("/Users/samlinsen/Dropbox/biodiversity/training/BootcampAI/01/trainedBananaModelTorch/bananaModel2.pt",  map_location=torch.device('cpu'))
-    #model = torch.load("/Users/samlinsen/Dropbox/biodiversity/training/BootcampAI/01/trainedBananaModelTorch/bananaModel_state_dict.pt",  map_location=torch.device('cpu'))
 
     #print(model)
     model.eval()
     print(model)
 
     normalize = T.Normalize(
-       mean=[0.485, 0.456, 0.406],
+       mean=[0.485, 0.456, 0.406],  #is this standard? I see the values multiple times, but should be consistent with training data
        std=[0.229, 0.224, 0.225]
     )
     preprocess = T.Compose([
-       T.Resize(256),
+       T.Resize(224),
        T.CenterCrop(224),
        T.ToTensor(),
        normalize
@@ -42,12 +40,16 @@ def main():
 
     layout = [
         [sg.Image(key="-IMAGE-")],
+        [sg.Text(size=(40, 1), key="-TOUT-")],
+
         [
             sg.Text("Image File"),
             sg.Input(size=(25, 1), key="-FILE-"),
-            sg.FileBrowse(file_types=file_types),
+            sg.FileBrowse("Get",file_types=file_types),
+            sg.Button("Clear"),
+            sg.Button("Show"),
             sg.Button("Predict"),
-            #sg.Text(prediction),
+    #        sg.Text(prediction),
         ],
     ]
 
@@ -57,7 +59,10 @@ def main():
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
-        if event == "Predict":
+        if event == "Clear":
+            #window["-IMAGE-"].Update('')
+            window.FindElement("-IMAGE-").Update('')
+        if event == "Show":
             filename = values["-FILE-"]
             if os.path.exists(filename):
                 image = Image.open(values["-FILE-"])
@@ -65,27 +70,22 @@ def main():
                 bio = io.BytesIO()
                 image.save(bio, format="PNG")
                 window["-IMAGE-"].update(data=bio.getvalue())
-                #response = values["-FILE-"]
-                #img_pil = Image.open(io.BytesIO(response))
+        if event == "Predict":
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                #image.thumbnail((254,254))
+                bio = io.BytesIO()
+                image.save(bio, format="PNG")
+                #window["-IMAGE-"].update(data=bio.getvalue())
                 img_tensor = preprocess(image)
                 img_tensor.unsqueeze_(0)
                 img_variable = Variable(img_tensor)
                 fc_out = model(img_variable)
-                print (fc_out)
-                #labels = {int(key):value for (key, value)
-                #    in requests.get(LABELS_URL).json().items()}
-                #print(labels[fc_out.data.numpy().argmax()])
-                #outputs = model(image)
-                #im = image_loader( values["-FILE-"] )
-                #pred, pred_idx, probs = model(im)
-                #prediction = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
-                #print(prediction)
+                print (labels[fc_out.data.numpy().argmax()])
+                window["-TOUT-"].update('life expectancy: ' + labels[fc_out.data.numpy().argmax()])
 
     window.close()
-
-
-
-
 
 
 
